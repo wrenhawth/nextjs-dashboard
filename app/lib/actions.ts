20c9135ffeabfca2425dd7614f782420ad1cffb8
Wren Hawthorne <wrenhawth@gmail.com>
2023-11-12 16:02:34 -0800

    Chapter 14 - part 1

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,22 +4,45 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres'
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+
 const InvoiceSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string(
+        { invalid_type_error: 'Please select a customer.' }
+    ),
+    amount: z.coerce
+        .number()
+        .gt(0, { message: 'Please enter an amount greater than $0.' }),
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: 'Please select an invoice status.'
+    }),
     date: z.string()
 })
 
 const CreateInvoice = InvoiceSchema.omit({ id: true, date: true })
 
-export async function createInvoice(formData: FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse({
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+}
+
+export async function createInvoice(prevState: State, formData: FormData) {
+    const validatedFields = CreateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status')
     })
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Cdreate Invoice'
+        }
+    }
+    const { customerId, amount, status } = validatedFields.data
     const amountInCents = amount * 100
     const date = new Date().toISOString().split('T')[0]
     try {
@@ -32,7 +55,7 @@ export async function createInvoice(formData: FormData) {
             message: 'Database Error: Failed to Create Invoice'
         }
     }
-  
+
     revalidatePath('/dashboard/invoices')
     redirect('/dashboard/invoices')
 }
@@ -64,13 +87,13 @@ export async function updateInvoice(id: string, formData: FormData) {
 
 export async function deleteInvoice(id: string) {
 
-  throw new Error('Failed to Delete Invoice');
-  
+    throw new Error('Failed to Delete Invoice');
+
     try {
-        await sql `DELETE FROM invoices WHERE id = ${id}`;
+        await sql`DELETE FROM invoices WHERE id = ${id}`;
         revalidatePath('/dashboard/invoices')
-        return { message: 'Deleted Invoice.'}
+        return { message: 'Deleted Invoice.' }
     } catch (error) {
-        return { message: 'Database Error: Failed to Delete Invoice.'}
+        return { message: 'Database Error: Failed to Delete Invoice.' }
     }
 }
